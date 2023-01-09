9b85ad9b586370a0f841ac07c91792ced6b60ca8
matinzd <zadehdolatabad@gmail.com>
2023-01-09 20:00:04 +0100

    feat: add docs website

---
+++ docs/docusaurus.config.js
@@ -0,0 +1,102 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'React Native Health Connect',
+  tagline: '',
+  url: 'https://matinzd.github.io',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  projectName: 'react-native-health-connect',
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'React Native Health Connect',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/health_connect_logo.png',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'get-started',
+            position: 'left',
+            label: 'Docs',
+          },
+          {
+            href: 'https://github.com/matinzd/react-native-health-connect',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Get started',
+                to: '/docs/get-started',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'GitHub',
+                href: 'https://github.com/matinzd/react-native-health-connect',
+              },
+            ],
+          },
+        ],
+        copyright: `Made with ❤️ by Matin Zadeh Dolatabad`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
