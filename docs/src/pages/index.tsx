61d72b8618b2fdb5113d58b70bd9557a4e6988d0
matinzd <zadehdolatabad@gmail.com>
2023-01-09 20:06:44 +0100

    fix: set default to intro

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -3,5 +3,5 @@ import React from 'react';
 import { Redirect } from '@docusaurus/router';
 
 export default function Home(): JSX.Element {
-  return <Redirect to="docs/get-started" />;
+  return <Redirect to="docs/intro" />;
 }
