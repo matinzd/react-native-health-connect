1fa62ef2b85d5e804049c930094f9ce0c676faed
matinzd <zadehdolatabad@gmail.com>
2023-03-29 01:51:53 +0200

    feat: add sleep session record

--- android/src/main/java/dev/matinzd/healthconnect/records/ReactSleepSessionRecord.kt
+++ android/src/main/java/dev/matinzd/healthconnect/records/ReactSleepSessionRecord.kt
@@ -6,21 +6,50 @@ import androidx.health.connect.client.request.AggregateRequest
 import com.facebook.react.bridge.ReadableArray
 import com.facebook.react.bridge.ReadableMap
 import com.facebook.react.bridge.WritableNativeMap
+import dev.matinzd.healthconnect.utils.*
+import java.time.Instant
 
 class ReactSleepSessionRecord : ReactHealthRecordImpl<SleepSessionRecord> {
   override fun parseWriteRecord(records: ReadableArray): List<SleepSessionRecord> {
-    TODO("Not yet implemented")
+    return records.toMapList().map { map ->
+      SleepSessionRecord(
+        startTime = Instant.parse(map.getString("startTime")),
+        endTime = Instant.parse(map.getString("endTime")),
+        startZoneOffset = null,
+        endZoneOffset = null,
+        title = map.getString("title"),
+        notes = map.getString("description"),
+      )
+    }
   }
 
   override fun parseRecord(record: SleepSessionRecord): WritableNativeMap {
-    TODO("Not yet implemented")
+    return WritableNativeMap().apply {
+      putString("startTime", record.startTime.toString())
+      putString("endTime", record.endTime.toString())
+      putString("title", record.title)
+      putString("notes", record.notes)
+      putMap("metadata", convertMetadataToJSMap(record.metadata))
+    }
   }
 
   override fun getAggregateRequest(record: ReadableMap): AggregateRequest {
-    TODO("Not yet implemented")
+    return AggregateRequest(
+      metrics = setOf(
+        SleepSessionRecord.SLEEP_DURATION_TOTAL
+      ),
+      timeRangeFilter = record.getTimeRangeFilter("timeRangeFilter"),
+      dataOriginFilter = convertJsToDataOriginSet(record.getArray("dataOriginFilter"))
+    )
   }
 
   override fun parseAggregationResult(record: AggregationResult): WritableNativeMap {
-    TODO("Not yet implemented")
+    return WritableNativeMap().apply {
+      putDouble(
+        "SLEEP_DURATION_TOTAL",
+        record[SleepSessionRecord.SLEEP_DURATION_TOTAL]?.seconds?.toDouble() ?: 0.0
+      )
+      putArray("dataOrigins", convertDataOriginsToJsArray(record.dataOrigins))
+    }
   }
 }
